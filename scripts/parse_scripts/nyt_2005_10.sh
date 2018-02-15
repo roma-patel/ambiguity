2b9e3d227579c665904b8c811dfab83e466eca0c
roma-patel <romapatel996@gmail.com>
2018-02-15 08:19:46 -0500

    added shell scripts

---
+++ scripts/parse_scripts/nyt_2005_10.sh
@@ -0,0 +1,3 @@
+source /home1/r/romap/crf/crf_task/bin/activate
+cd /home1/c/cis530/hw3/corenlp/stanford-corenlp-2012-07-09
+java -Xmx20g -cp stanford-corenlp-2012-07-09.jar:stanford-corenlp-2012-07-06-models.jar:xom.jar:joda-time.jar -Xmx3g edu.stanford.nlp.pipeline.StanfordCoreNLP -annotators tokenize,ssplit,pos,parse -file /nlp/data/romap/ambig/docs/nyt_2005_10.txt -outputDirectory /nlp/data/romap/ambig/parse/
